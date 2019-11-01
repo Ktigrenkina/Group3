1b362dc38e378257b16730e35715f2f53f9e65b7
Dima <geliosnda@gmail.com>
2019-11-01 17:04:24 +0700

    Dima

--- proba V 2/proba V 2/Form1.cs
+++ proba V 2/proba V 2/Form1.cs
@@ -16,5 +16,10 @@ namespace proba_V_2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+        }
     }
 }
