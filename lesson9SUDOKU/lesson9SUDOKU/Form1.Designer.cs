c82f848f52ced1ccd6685610fff07e4e56fe2535
Narteno <49160330+Narteno@users.noreply.github.com>
2019-11-03 15:41:45 +0700

    sudoku
    v0

---
+++ lesson9SUDOKU/lesson9SUDOKU/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace lesson9SUDOKU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 650);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(495, 60);
+            this.button1.TabIndex = 82;
+            this.button1.Text = "Проверить решение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(490, 490);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 83;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(25F, 51F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 722);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(9);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ lesson9SUDOKU/lesson9SUDOKU/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson9SUDOKU
+{
+    public partial class Form1 : Form
+    {
+        TextBox tb;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // size 50 56
+            // start loc 13 13
+            // x на 56
+            // y на 62
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int IndexOfTb = 0, y = 17;
+
+            for (int i = 0; i < 9; i++)
+            {
+                int x = 16;
+                for (int j = 0; j < 9; j++)
+                {
+                    tb = new TextBox()
+                    {
+                        Name = "textBox" + IndexOfTb,
+                        Size = new Size(51, 51),
+                        Location = new Point(x, y),
+                        TextAlign = HorizontalAlignment.Center,
+                        Multiline = true,
+                        BorderStyle = BorderStyle.None,
+                        Font = new Font("Microsoft Sans Serif",32)
+                        //Text = "textBox" + IndexOfTb
+                    };
+                    tb.TextChanged += tb_TextChanged;
+                    tb.KeyPress += tb_KeyPress;
+                    Controls.Add(tb);
+                    IndexOfTb++;
+                    if ((j + 1) % 3 == 0)
+                        x += 57;
+                    else
+                        x += 53;
+                }
+                if ((i + 1) % 3 == 0)
+                    y += 56;
+                else
+                    y += 53;
+            }
+            pictureBox1.SendToBack();
+        }
+
+        private void tb_TextChanged(object sender, EventArgs e)
+        {
+            tb = sender as TextBox;
+            if (tb.Text.Length > 1)
+            {
+                tb.Text = tb.Text.Substring(0, 1);
+            }
+        }
+
+        private void tb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
